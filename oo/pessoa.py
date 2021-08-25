38cd2a34181711fdab2194cfed803b57f9f64c2e
Harold Gautschi <telemac.dev@gmail.com>
2021-08-25 13:30:07 -0400

    atributo de class

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,9 @@
 class Pessoa:
+    # atributo de class
+    olhos = 2
+
+    # atributos de instância
+    # função dunder init __init__ (para inicialização dos atributo de instância)
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -23,6 +28,16 @@ if __name__ == '__main__':
     # Apagar um atributo
     del jordan.filhos
     # Mostrar os atributos de instancia objeto.__dict__
+    print(harold.__dict__)
+    print(jordan.__dict__)
+    # chamar atributo de class
+    print(Pessoa.olhos)
+    # modificando o atributo de class, ele passa a ser um atributo do objeto
+    harold.olhos = 1
+    # acesso no atributo de class pelo objeto
+    print(harold.olhos)
+    print(jordan.olhos)
+    print(id(Pessoa.olhos), id(harold.olhos), id(jordan.olhos))
     print(harold.__dict__)
     print(jordan.__dict__)
 
